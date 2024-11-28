2501be448aebb3686556fdc533196afcf0813841
Saidov Sarvarbek <forgivensinner6@gmail.com>
2024-11-28 20:14:53 +0500

    commit

--- foods/views.py
+++ foods/views.py
@@ -1,33 +1,31 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.filters import SearchFilter, OrderingFilter
+from django.shortcuts import get_object_or_404
+from rest_framework import generics
 from .models import FoodType, Food, Comment
 from .serializers import FoodTypeSerializer, FoodSerializer, CommentSerializer
-# Create your views here.
 
-class FoodTypeViewSet(ModelViewSet):
+class FoodTypeListView(generics.ListCreateAPIView):
     queryset = FoodType.objects.all()
     serializer_class = FoodTypeSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ['name']
-    ordering_fields = ['id', 'name']
 
-class FoodViewSet(ModelViewSet):
+
+class FoodTypeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = FoodType.objects.all()
+    serializer_class = FoodTypeSerializer
+
+class FoodListView(generics.ListCreateAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ['name', 'description']
-    ordering_fields = ['id', 'name', 'price']
 
-class CommentViewSet(ModelViewSet):
+
+class FoodDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+
+class CommentListView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [SearchFilter, OrderingFilter]
-    search_fields = ['content', 'author__username']
-    ordering_fields = ['id', 'created_at']
 
 
-
+class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
