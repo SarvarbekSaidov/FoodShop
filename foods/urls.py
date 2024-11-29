1f0119c0f5fb48be68959015192dbc0552f925a5
Saidov Sarvarbek <forgivensinner6@gmail.com>
2024-11-29 14:49:22 +0500

    Usage of ModelViewSet

--- foods/urls.py
+++ foods/urls.py
@@ -1,13 +1,9 @@
-from django.urls import path
-from .views import (
-    FoodTypeListView, FoodTypeDetailView, FoodListView, FoodDetailView, CommentListView, CommentDetailView
-)
+from rest_framework import routers
+from .views import FoodTypeListView, FoodListView, CommentAPIViewSet
 
-urlpatterns = [
-    path('food-types/', FoodTypeListView.as_view(), name='foodtype-list'),
-    path('food-types/<int:pk>/', FoodTypeDetailView.as_view(), name='foodtype-detail'),
-    path('foods/', FoodListView.as_view(), name='food-list'),
-    path('foods/<int:pk>/', FoodDetailView.as_view(), name='food-detail'),
-    path('comments/', CommentListView.as_view(), name='comment-list'),
-    path('comments/<int:pk>/', CommentDetailView.as_view(), name='comment-detail'),
-]
+router = routers.DefaultRouter()
+router.register('food-types', FoodTypeListView, basename='foodtype')
+router.register('foods', FoodListView, basename='food')
+router.register('comments', CommentAPIViewSet, basename='comment')
+
+urlpatterns = router.urls
--- foods/views.py
+++ foods/views.py
@@ -2,33 +2,18 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from .models import FoodType, Food, Comment
 from .serializers import FoodTypeSerializer, FoodSerializer, CommentSerializer
-
-class FoodTypeListView(generics.ListCreateAPIView):
-    queryset = FoodType.objects.all()
-    serializer_class = FoodTypeSerializer
+from rest_framework.viewsets import ModelViewSet
+from .permissions import IsAuthorOrReadOnly
 
 
-class FoodTypeDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = FoodType.objects.all()
-    serializer_class = FoodTypeSerializer
-
-class FoodListView(generics.ListCreateAPIView):
+class FoodTypeListView(ModelViewSet):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
 
-
-class FoodDetailView(generics.RetrieveUpdateDestroyAPIView):
+class FoodListView(ModelViewSet):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
-<<<<<<< Updated upstream
-=======
-    permission_classes = [IsAuthorOrReadOnly]
->>>>>>> Stashed changes
 
-class CommentListView(generics.ListCreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
+class CommentAPIViewSet(ModelViewSet):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
